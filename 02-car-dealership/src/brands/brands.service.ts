e092feac179fe3ef4ee2fcc28f78f9a2b9bb03b5
ilima12091 <limaignacio12091@gmail.com>
2025-12-31 17:16:50 -0300

    Se agrega el recurso de brands

---
+++ 02-car-dealership/src/brands/brands.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBrandDto } from './dto/create-brand.dto';
+import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class BrandsService {
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Toyota',
+      createdAt: Date.now(),
+    },
+  ];
+
+  create(createBrandDto: CreateBrandDto) {
+    const { name } = createBrandDto;
+
+    const brand: Brand = {
+      id: uuid(),
+      name: name.toLocaleLowerCase(),
+      createdAt: Date.now(),
+    };
+
+    this.brands.push(brand);
+
+    return brand;
+  }
+
+  findAll() {
+    return this.brands;
+  }
+
+  findOne(id: string) {
+    const brand = this.brands.find((item) => item.id === id.toString());
+
+    if (!brand) {
+      throw new Error(`Brand with id ${id} not found`);
+    }
+
+    return brand;
+  }
+
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    let brandDB = this.findOne(id);
+
+    this.brands = this.brands.map((brand) => {
+      if (brand.id === id.toString()) {
+        brandDB = { ...brandDB, ...updateBrandDto, updatedAt: Date.now() };
+        return brandDB;
+      }
+      return brand;
+    });
+
+    return brandDB;
+  }
+
+  remove(id: string) {
+    if (!this.findOne(id)) {
+      throw new Error(`Brand with id ${id} not found`);
+    }
+
+    this.brands = this.brands.filter((brand) => brand.id !== id.toString());
+
+    return true;
+  }
+}
