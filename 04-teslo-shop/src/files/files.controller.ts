66505d0b59e1a9e6a8d88d61d0757ad1a4f80acf
ilima12091 <limaignacio12091@gmail.com>
2026-01-06 22:49:02 -0300

    Se agrega una forma de servir imágenes estáticas

---
+++ 04-teslo-shop/src/files/files.controller.ts
@@ -0,0 +1,57 @@
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FilesService } from './files.service';
+import { diskStorage } from 'multer';
+import { fileFilter, fileNamer } from './helpers';
+import type { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+
+@Controller('files')
+export class FilesController {
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Post('products')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter,
+      limits: {
+        fileSize: 1024 * 1024 * 10,
+      },
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
+  uploadProductFile(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('File not provided or invalid file type');
+    }
+
+    const secureUrl = `${this.configService.get('HOST_API')}/files/products/${file.filename}`;
+
+    return secureUrl;
+  }
+
+  @Get('products/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+
+    res.sendFile(path);
+  }
+}
--- 04-teslo-shop/src/main.ts
+++ 04-teslo-shop/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Main');
+
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,7 +14,9 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  logger.log(`Application running on port ${port}`);
 }
 
 bootstrap();
